18512066c3990af852fcbfda98df705f2d635ad0
zassory <nicolas.programador@gmail.com>
2023-01-09 13:11:35 -0300

    Variables

---
+++ variables.py
@@ -0,0 +1,23 @@
+name = "Nicolas";
+name2 = None;
+
+x,book = 100, "I Robot";
+
+#Case sensitive
+#book = "Digital Fortress";
+#Book = "Digital Fortress";
+
+print(name);
+print(name2);
+print(book);
+#print(Book);
+print(x);
+print(book);
+
+# Conventions
+book_name = "I Robot" #Snake Case
+bookName = "Digital Fortress" # Camel Case
+BookName = "Papelucho" #Pascal Case
+
+PI = 3.1416 #Mayuscula no cambia, es constante
+MY_NAME = "Nicolas";
