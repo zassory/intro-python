6eda7102356b1e147186a69cabd73a34ae90149e
zassory <nicolas.programador@gmail.com>
2023-01-09 13:42:03 -0300

    Variables string , propiedades y funciones

---
+++ strings.py
@@ -0,0 +1,30 @@
+myStr = "Nicolas";
+print("My name is "+ myStr);
+print(f"My name is {myStr}");
+print("My name is {0}".format(myStr));
+#print(dir(myStr));
+
+# print(myStr.upper());
+# print(myStr.lower());
+# print(myStr.swapcase());
+# print(myStr.capitalize());
+# print(myStr.replace('Hello','Hola').upper());
+# print(myStr.count('l'));
+# print(myStr.count('H'));
+# print(myStr.startswith('Hello'));
+# print(myStr.endswith('World'));
+# print(myStr.endswith('Megaman'));
+# print(myStr.split());
+# print(myStr.split('o'));
+# print(myStr.find('o'));#Posicion
+
+# print(len(myStr));
+# print(myStr.index('e'));
+# print(myStr.isnumeric());
+# print(myStr.isalpha());#alfanumerico
+#print(myStr[4]);
+# print(myStr[0]);
+# print(myStr[3]);
+# print(myStr[4]);
+# print(myStr[-1]);
+# print(myStr[-5]);
